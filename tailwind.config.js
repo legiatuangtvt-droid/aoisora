fe4d93f0802b76fd11cd1a5b38828b292434e899
DESKTOP-QIGPSLD\WIN10 <legiatuangtvt@gmail.com>
2025-12-02 14:04:44 +0700

    react-app

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./public/**/*.html",
-    "./public/**/*.js"
+    // Quét tất cả các file HTML và JS trong thư mục public
+    './public/**/*.html',
+    './public/**/*.js',
+
+    // Quét tất cả các file JSX và TSX trong thư mục src của ứng dụng React
+    './react-app/src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     extend: {},
